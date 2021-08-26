9d50a83507af2eaca85f46cfc8c90aec40063651
Raz Levi <66571396+razlevio@users.noreply.github.com>
2021-08-26 15:14:47 +0300

    Create tests.java

---
+++ 7-kyu/Growth of a Population/tests.java
@@ -0,0 +1,44 @@
+public class tests {
+
+    private static int randInt(int min, int max) {
+        return min + (int)(Math.random() * ((max - min) + 1));
+    }
+    private static void testing(int actual, int expected) {
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test1() {
+        System.out.println("Fixed Tests: nbYear");    
+        testing(solution.nbYear(1500, 5, 100, 5000),15);
+        testing(solution.nbYear(1500000, 2.5, 10000, 2000000), 10);
+        testing(solution.nbYear(1500000, 0.25, 1000, 2000000), 94);
+        testing(solution.nbYear(1500000, 0.25, -1000, 2000000), 151);
+        testing(solution.nbYear(1500000, 0.25, 1, 2000000), 116);
+        testing(solution.nbYear(1500000, 0.0, 10000, 2000000), 50);
+    }
+    //............
+    private static int nbYearSol(int p0, double percent, int aug, int p) {
+        int i = 1;
+        double mult = 1 + percent / 100.0;
+        double prev = p0;
+        while (prev < p) {
+            double ne = Math.floor(prev * mult + aug);
+            prev = ne;
+            i++;
+        }
+        return (i - 1);
+    }
+    //............
+    @Test
+    public void test4() {
+        System.out.println("Random Tests");        
+        for (int i = 0; i < 100; i++) { 
+            int p0 = randInt(10000, 15000000);
+            double percent = randInt(50, 1000) / 100.0;
+            int aug = (int)(p0 / 200.0);
+            int k = randInt(5, 100);
+            int p = p0 + k * aug; 
+            testing(solution.nbYear(p0, percent, aug, p), nbYearSol(p0, percent, aug, p)); 
+        }
+    }
+}
